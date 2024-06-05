e470ef9ebdc2c3f6a91c330b23da86a60172ab19
AhhmedJamal <ahhmed.jamal@gmail.com>
2024-06-05 13:29:13 +0300

    update

--- server.js
+++ server.js
@@ -2,23 +2,23 @@ const express = require("express");
 const Stripe = require("stripe");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 const app = express();
 const stripe = Stripe(
   "sk_test_51Oa7BfHIYZ0Ho4vpxjDD2XmRNXFEgSZiTOkHdy98pMVTCDZsef8Vx1fIfA3x3O2OOo2fXksyhXAq8TeTWgWHiyDc00SNO7TMAE"
-);
+); // Replace with your Stripe secret key
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/payment", async (req, res) => {
+app.post("/create-payment-intent", async (req, res) => {
   const { amount } = req.body;
-  console.log(amount);
+
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: "usd",
+      currency: "EPG",
     });
+
     res.status(200).send({
       clientSecret: paymentIntent.client_secret,
     });
@@ -27,4 +27,4 @@ app.post("/payment", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3001, () => console.log("Server running on port 3001"));
